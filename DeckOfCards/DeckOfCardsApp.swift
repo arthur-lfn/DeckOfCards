ec4b7252121d4dc4faa2a4a6db75e8b45f4eba44
Arturo Alfani <aalfani22@fed.idserve.net>
2023-07-03 10:27:55 +0200

    Initial Commit

---
+++ DeckOfCards/DeckOfCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeckOfCardsApp.swift
+//  DeckOfCards
+//
+//  Created by Arturo Alfani on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DeckOfCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
